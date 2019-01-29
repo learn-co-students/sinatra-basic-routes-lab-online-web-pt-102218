713975d216d4bb23bffc7355df0c6c8f01a1d049
Elyse Burmester <burmesterelyse@gmail.com>
2019-01-29 02:48:34 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get '/name' do 
+    "My name is Elyse"
+  end 
+  
+  get '/hometown' do 
+    "My hometown is Oviedo, Florida"
+  end
+  
+  get '/favorite-song' do 
+    "My favorite song is Quarter Past Midnight by Bastille"
+  end
 end
