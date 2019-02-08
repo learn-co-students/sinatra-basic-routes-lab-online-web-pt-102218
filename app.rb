a4c55626dacdd246669ccf5a40d276cfdafb2a72
Joshua D. Forrest <joshuadforrest@gmail.com>
2019-02-08 16:15:46 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/name' do
+    "My name is Josh"
+  end
+  
+  get '/hometown' do
+    "My hometown is Winter Haven FL"
+  end
+  
+  get '/favorite-song' do
+    "My favorite song is 'I Will Still Love You' by Isaac Shepard"
+  end
+  
 end
