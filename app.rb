6e3e1146b78fd1ce7851093b092e46aaee8d1c36
Corina Talavera <corina.talavera@gmail.com>
2019-02-12 23:23:09 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,18 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    "Hello, World!"
+  end
+  get '/name' do
+    "My name is Corina"
+  end
+  get '/hometown' do
+    "My hometown is Old Bridge"
+  end
+  get '/favorite-song' do
+    "My favorite song is Rise Up"
+  end
+
 end
