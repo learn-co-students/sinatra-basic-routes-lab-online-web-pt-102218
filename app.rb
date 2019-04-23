61b3f598e236a47616304ea8f0068a27f62c2f7c
Leslie Osorio <lesosorio17@gmail.com>
2019-04-23 20:08:57 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Leslie"
+  end
+
+  get '/hometown' do
+    "My hometown is The Valley"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is The Climb"
+  end 
 end
